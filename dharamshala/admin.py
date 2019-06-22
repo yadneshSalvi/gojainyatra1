4e4dc3fb26fbc28d85e88b12ea0181993778e5de
yadneshSalvi <yadneshujwal@gmail.com>
2019-06-22 21:08:35 +0530

    added feedbacks to admin

--- dharamshala/admin.py
+++ dharamshala/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Shala,vlog,Booking
+from .models import Shala,vlog,Booking,Feedback
 
 # Register your models here.
 
 admin.site.register(Shala)
 admin.site.register(vlog)
 admin.site.register(Booking)
+admin.site.register(Feedback)
